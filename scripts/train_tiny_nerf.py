2a2f7ac39c2e77aa4a317700d4935b47c205ca40
Grace-11 <wonderful00grace@gmail.com>
2023-07-21 20:14:51 +0900

    Arg parse added

--- scripts/train_tiny_nerf.py
+++ scripts/train_tiny_nerf.py
@@ -2,17 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from tqdm import tqdm
+from pathlib import Path
+from datetime import datetime
+from argparse import ArgumentParser
 
 from torch import nn, optim
 
 from nerfdiff.nerf.tiny_nerf import TinyNeRF
 
-def load_data(device):
-    data_f = "/usr/dataset/cars.npz"
-    # data_f = "/usr/dataset/lego.npz"
-    data = np.load(data_f)
 
-    images = data["images"] / 255
+def load_data(device):
+    # data_f = "/usr/dataset/cars.npz"
+    data_f = "/usr/dataset/lego.npz"
+    data = np.load(data_f)
+    print(data)
+    # print(data['camera_distance'])
+    # print(data['poses'])
+
+    # images = data["images"] / 255
+    images = data['images']
     img_size = images.shape[1]
     #TODO here image_size is used for x only
     xs = torch.arange(img_size) - (img_size / 2 - 0.5)
@@ -24,49 +32,87 @@ def load_data(device):
     camera_coords = pixel_coords / focal
 
     init_ds = camera_coords.to(device)
-    init_o = torch.Tensor(np.array([0, 0, float(data["camera_distance"])])).to(device)
+    # init_o = torch.Tensor(np.array([0, 0, float(data["camera_distance"])])).to(device)
+    # print(init_o)
+    print(init_ds.shape)
+    
 
     #TODO no point in returning img_size here!!! ???
-    return (images, data["poses"], init_ds, init_o, img_size)
+    return (images, data["poses"], init_ds)
 
 
-def set_up_test_data(images, device, poses, init_ds, init_o):
+def set_up_test_data(images, device, poses, init_ds):
     test_idx = 0
     test_img = torch.Tensor(images[test_idx]).to(device)
     test_R = torch.Tensor(poses[test_idx, :3, :3]).to(device)
     test_ds = torch.einsum("ij,hwj->hwi", test_R, init_ds)
-    test_os = (test_R @ init_o).expand(test_ds.shape)
+    # test_os = (test_R @ init_o).expand(test_ds.shape)
+    test_os = torch.Tensor(poses[test_idx, :3, 3]).to(device).expand(test_ds.shape)
     train_idxs = np.arange(len(images)) != test_idx
 
     return (test_ds, test_os, test_img, train_idxs)
 
+def get_timestamp():
+    return datetime.now().strftime("%Y-%m-%d_%H%M%S")
+
 
 if __name__ == "__main__":
     #TODO add argparse arguments for: device, dataset path, seed etc.. (that needs change)
-    seed = 9458
-    torch.manual_seed(seed)
-    np.random.seed(seed)
 
-    device = "cuda:0"
+    parser = ArgumentParser(description='Train Tiny NeRF')
+    parser.add_argument('--seed', type=int )
+    parser.add_argument('--use-cpu', action='store_true')
+    parser.add_argument('--data-dir', type=Path, required=True)
+    parser.add_argument('--output-dir', type=Path, required=True)
+    parser.add_argument('--scene-name', type=str)
+    parser.add_argument('--experiment-name', type=str, default=get_timestamp())
+    parser.add_argument('--num-iterations', type=int, default=20000)
+    parser.add_argument('--steps-per-save', type=int, default=1000)
+    parser.add_argument('--steps-per-eval', type=int, default=1000)
+    parser.add_argument('--lr', type=float, default=5e-3)
+    args = parser.parse_args()
+    print(args)
+
+    seed = args.seed
+    if(seed):
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+    device = 'cpu' if args.use_cpu else 'cuda:0'
+    data_dir = args.data_dir
+    output_dir = args.output_dir
+    scene = args.scene_name or data_dir.name
+    experiment_name = args.experiment_name
+    num_iterations = args.num_iterations
+    steps_per_save = args.steps_per_save
+    steps_per_eval = args.steps_per_eval
+    lr= args.lr
+    assert steps_per_save < num_iterations, f'Steps per save should not be greater than number of iterations ({steps_per_save} > {num_iterations})'
+    assert steps_per_eval < num_iterations, f'Steps per evaluation should not be greater than number of iterations ({steps_per_eval} > {num_iterations})'
+
+
+    experiment_save_path = output_dir / scene / experiment_name
+    model_save_path = experiment_save_path / 'models'
+    log_save_path = experiment_save_path / 'logs'
+
+    model_save_path.mkdir(parents=True, exist_ok=True)
+    log_save_path.mkdir(parents=True, exist_ok=True)
+
+
     nerf = TinyNeRF(device)
-
-    lr = 5e-3
     optimizer = optim.Adam(nerf.F_c.parameters(), lr=lr)
     criterion = nn.MSELoss()
 
-    (images, poses, init_ds, init_o, test_img) = load_data(device)
+    (images, poses, init_ds) = load_data(device)
     (test_ds, test_os, test_img, train_idxs) = set_up_test_data(
-        images, device, poses, init_ds, init_o
+        images, device, poses, init_ds
     )
     images = torch.Tensor(images[train_idxs])
     poses = torch.Tensor(poses[train_idxs])
 
     psnrs = []
     iternums = []
-    num_iters = 20000
-    display_every = 1000
     nerf.F_c.train()
-    for i in tqdm(range(num_iters)):
+    for i in tqdm(range(num_iterations)):
         target_img_idx = np.random.randint(images.shape[0])
         target_pose = poses[target_img_idx].to(device)
         R = target_pose[:3, :3]
@@ -81,10 +127,12 @@ if __name__ == "__main__":
         loss.backward()
         optimizer.step()
 
-        if i % display_every == 0:
-            torch.save(nerf.F_c.state_dict(), f"/usr/nerfdiff/nerf/model_{i}.pth")
+
+        if i % steps_per_save == 0:
+            torch.save(nerf.F_c.state_dict(), model_save_path / f"model_{i}.pth")
 
             print("Saving")
+        if i % steps_per_eval == 0:
             nerf.F_c.eval()
             with torch.no_grad():
                 C_rs_c = nerf(test_ds, test_os)
@@ -104,11 +152,11 @@ if __name__ == "__main__":
             plt.plot(iternums, psnrs)
             plt.title("PSNR")
             # plt.show()
-            plt.savefig(f"out2/{i}.jpg")
+            plt.savefig(log_save_path / f"{i}.jpg")
             plt.close()
 
             nerf.F_c.train()
 
     
-    torch.save(nerf.F_c.state_dict(), f"/usr/nerfdiff/nerf/model_last.pth")
+    torch.save(nerf.F_c.state_dict(),model_save_path /  f"model_last.pth")
     print("Done!")
